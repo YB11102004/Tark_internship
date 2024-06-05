3aa1487a68c1634aaad33e8880ff5d6673220b2e
YB11102004 <132495939+YB11102004@users.noreply.github.com>
2024-06-05 14:29:13 +0530

    4th.cpp

--- 4th.cpp
+++ 4th.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main() {
     int pos[4]={0, 0, 0, 0};
     int len[4]={2, 4, 8, 16};
+    int n=sizeof(pos)/sizeof(pos[0]);
     string s;
-    cin>>s; // "XY"
-    for(int i=0;i<4;i++)
+    cin>>s;
+    for(int i=0;i<n;i++)
     {
         string s1=s.substr(pos[i],len[i]);
         reverse(s1.begin(),s1.end());
